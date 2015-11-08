4d1a8d744688012f900d5cea50aae6ad52fd3194
Kumsal Obuz <iam@denizkumsal.com>
2015-11-07 22:41:38 -0500

    Data on the webpage has been fixed. Finishes article regex

--- databases.py
+++ databases.py
@@ -2,14 +2,13 @@ import urllib.request
 import urllib.parse
 import urllib.response
 import re
-
+import sys
 
 with urllib.request.urlopen("http://nosql-database.org/") as response:
     html = response.read()
 
-#clean up html from \n and \t
-html = html.decode().replace('\n','').replace('\t','')
-
+#clean up html from \n and \t and double spaces
+html = html.decode().replace('\n','').replace('\t','').replace('  ','')
 
 #each h2 is a group of NoSQL database
 categories = re.findall("<h2>(.*?)</h2>", html)
@@ -18,8 +17,9 @@ for category in categories:
     print(category)
     #each category is followed by a number of articles and these articles are superceded by a final sectin before the next category
     category_content = re.findall("<h2>"+category+"<\/h2>(<article>.*?<\/article>)<\/section>", html)
-    articles = re.findall("(<article>.*?</article>)", category_content)
 
-    print(len(articles))
+    articles = re.findall("(<article>.*?</article>)", str(category_content).strip())
+
+
     for article in articles:
         print(article)
