9e6c39e236010a96b4ae1591dc609a253530f32d
Kumsal Obuz <iam@denizkumsal.com>
2015-11-23 12:58:15 -0500

    Implements neo4j graph creation

--- databases.py
+++ databases.py
@@ -1,11 +1,20 @@
 import urllib.request
 import json
 from bs4 import BeautifulSoup
+from py2neo import *
 
 with urllib.request.urlopen("http://nosql-database.org/") as response:
     html = BeautifulSoup(response.read(), "html.parser")
 
 nosql_databases = {}
+
+'''
+I'm starting the Graph database by authentication and cleaning up old data
+'''
+authenticate("localhost:7474", "neo4j", "old4j")
+graph = Graph()
+graph.cypher.execute("MATCH (n) DETACH DELETE n")
+
 
 #each h2 is a group of NoSQL database
 categories = html.find_all("h2")
@@ -14,6 +23,9 @@ for category in categories:
 
     category_id = str(category.text).replace(" ","")
     nosql_databases[category_id] = {"name":category.text, "entries":[]}
+
+    #plot the node for category
+    category_node = Node("Category", name=category.text)
 
     '''
     By first finding h2 tags in html we actually got rid of the extra section at the beginning
@@ -38,6 +50,12 @@ for category in categories:
         entry = {'name':database_name, 'url':database_url, 'content':database_content}
 
         nosql_databases[category_id]["entries"].append(entry)
+
+        #plot the entry-category relationship
+        entry_node = Node("Database", name=database_name)
+        entry_category_relationship = Relationship(entry_node, "BELONGS TO", category_node)
+        #for each entry we must register the relationship in the graph
+        graph.create(entry_category_relationship)
 
 
 json.dump(nosql_databases, open("dump.json","w"), indent="\t", sort_keys=True)
