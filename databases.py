47f16196fcbcdddfc8662c734a0ed023776a3f99
Kumsal Obuz <iam@denizkumsal.com>
2015-11-22 22:04:44 -0500

    Implements BeautifulSoup to parse http://nosql-database.org/ content

--- databases.py
+++ databases.py
@@ -1,23 +1,43 @@
-import sys
 import urllib.request
+import json
 from bs4 import BeautifulSoup
 
 with urllib.request.urlopen("http://nosql-database.org/") as response:
     html = BeautifulSoup(response.read(), "html.parser")
 
+nosql_databases = {}
 
 #each h2 is a group of NoSQL database
 categories = html.find_all("h2")
 
 for category in categories:
-    print(category)
 
+    category_id = str(category.text).replace(" ","")
+    nosql_databases[category_id] = {"name":category.text, "entries":[]}
 
-    databases = category.parent.find_all("h3")
+    '''
+    By first finding h2 tags in html we actually got rid of the extra section at the beginning
+    Therefore we have to go up one level with 'parent' and seek article tags
+    Another alternative would be finding the sibling of h2 or category but there might be more tags other than
+    article so this method is safer
+    '''
+    databases = category.parent.find_all("article")
     for database in databases:
-        database_tag = database.parent
 
-        if database.find("a"):
-            print(database_tag)
+        #some entries don't have much details than just a mention to a product name
+        if database.find("h3"):
+            database_name = database.find("h3").text
         else:
-            print(database.text)
+            continue
+
+        database_url = database.find("a").get('href')
+        #by getting rid of h3 completely the whole article tag is now holding the content for the database
+        database.h3.extract()
+        database_content = database.text.strip()
+
+        entry = {'name':database_name, 'url':database_url, 'content':database_content}
+
+        nosql_databases[category_id]["entries"].append(entry)
+
+
+json.dump(nosql_databases, open("dump.json","w"), indent="\t", sort_keys=True)
